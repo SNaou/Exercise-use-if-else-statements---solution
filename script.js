a104e923e8ab720bff93ccd24d055f08b64f236f
SNaou <S.Naou@icloud.com>
2023-01-09 16:49:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+const age = 18;
+
+if (age >= 18) {
+  console.log(" You are older than 18 Acces Accepted.");
+}
+
+else {
+    console.log("Sorry, you are too young and are kindly requested to stay outside.");
+  }
+
+const isFemale = true;
+
+if (isFemale) {
+  console.log("Welcome to the ladies night!");
+
+} else {
+  console.log("Sorry, this event is for females only.");
+}
+
+const driverStatus = "designated";
+
+if (driverStatus === "designated") {
+  console.log("You are the designated driver and are allowed to drive.");
+
+
+} else {
+  console.log("Sorry, you are not the designated driver and are not allowed to drive.");
+}
